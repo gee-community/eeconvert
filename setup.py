850ff486630d2c6ea8d4513996e6fc6c37e230e5
rutgerhofste <rutgerhofste@gmail.com>
2018-01-04 17:19:39 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='eeconvert',
+      version='0.1',
+      description='earth engine conversion for jupyter notebook use',
+      url='http://github.com/rutgerhofste/eeconvert',
+      author='Rutger Hofste',
+      author_email='rutgerhofste@gmail.com',
+      license='MIT',
+      packages=['eeconvert'],
+      zip_safe=False)
