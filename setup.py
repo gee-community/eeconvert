dda56ccf742d5e5772b4747968d803c01c966bbf
rutgerhofste <rutgerhofste@gmail.com>
2018-01-08 14:30:38 -0500

    version 1.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='eeconvert',
-      version='0.1.1',
+      version='0.1.2',
       description='earth engine conversion for jupyter notebook use',
       url='http://github.com/rutgerhofste/eeconvert',
       author='Rutger Hofste',
