9f07413895ffb4b87632dd66c4ac1d3b59fe535e
rutgerhofste <rutgerhofste@gmail.com>
2018-01-08 15:06:12 -0500

    added dependency for database connect

---
+++ build/lib/eeconvert/__init__.py
@@ -0,0 +1,28 @@
+def rdsConnect(database_identifier,database_name):
+    """open a connection to AWS RDS
+    
+    in addition to specifying the arguments you need to store your password in a file called .password in the current working directory. 
+    You can do this using the command line or Jupyter. Make sure to have your .gitignore file up to date.
+    
+    Args:
+        database_identifier (string) : database identifier used when you set up the AWS RDS instance
+        database_name (string) : the database name to connect to
+        
+    Returns:
+        engine (sqlalchemy.engine.base.Engine) : database engine
+        connection (sqlalchemy.engine.base.Connection) : database connection
+    """
+    
+    
+    rds = boto3.client('rds')
+    F = open(".password","r")
+    password = F.read().splitlines()[0]
+    F.close()
+    response = rds.describe_db_instances(DBInstanceIdentifier="%s"%(database_identifier))
+    status = response["DBInstances"][0]["DBInstanceStatus"]
+    print("Status:",status)
+    endpoint = response["DBInstances"][0]["Endpoint"]["Address"]
+    print("Endpoint:",endpoint)
+    engine = sqlalchemy.create_engine('postgresql://rutgerhofste:%s@%s:5432/%s' %(password,endpoint,database_name))
+    connection = engine.connect()
+    return engine, connection
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='eeconvert',
-      version='0.1.2',
+      version='0.1.3',
       description='earth engine conversion for jupyter notebook use',
       url='http://github.com/rutgerhofste/eeconvert',
       author='Rutger Hofste',
       author_email='rutgerhofste@gmail.com',
       license='MIT',
       packages=['eeconvert'],
-      install_requires=[],
+      install_requires=[boto3,botocore],
       zip_safe=False)
