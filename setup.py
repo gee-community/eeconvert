b40818b05ea811f05e7587d6375f7a8f3e00bace
Rutger Hofste <rutgerhofste@gmail.com>
2018-01-09 11:02:13 -0500

    added geopandas as dependency

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='eeconvert',
       author_email='rutgerhofste@gmail.com',
       license='MIT',
       packages=['eeconvert'],
-      install_requires=['boto3','botocore','sqlalchemy','earthengine-api','shapely','folium','geojson','branca','pandas'],
+      install_requires=['boto3','botocore','sqlalchemy','earthengine-api','shapely','folium','geojson','branca','pandas','geopandas'],
       zip_safe=False)
